4f208084dcb63e18e33c857117457c595dc3c230
YAMAMOTO Takashi <imuwoto@gmail.com>
2021-05-08 08:57:02 +0900

    test-dind.sh: test multiple platforms

--- test/test-dind.sh
+++ test/test-dind.sh
@@ -5,5 +5,17 @@ docker buildx version
 
 cd $(dirname $0)
 cd alpine
-docker buildx build -t test .
-docker run test
+
+PLATFORMS="linux/amd64 linux/arm64"
+
+for P in $PLATFORMS; do
+    docker buildx build \
+        --platform $P \
+        -t test-$P \
+        --load \
+        .
+done
+
+for P in $PLATFORMS; do
+    docker run test-$P
+done
