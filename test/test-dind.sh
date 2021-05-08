b7227abcc30df6e7beb4f50d99954f22deea85e8
YAMAMOTO Takashi <imuwoto@gmail.com>
2021-05-08 09:17:15 +0900

    test-dind.sh: run simpler tests first

--- test/test-dind.sh
+++ test/test-dind.sh
@@ -3,10 +3,18 @@
 docker version
 docker buildx version
 
+PLATFORMS="linux/amd64 linux/arm64"
+
+for P in $PLATFORMS; do
+    NAME=alpine:3.12.0
+    docker run \
+        --platform $P \
+        ${NAME} \
+        uname -a
+done
+
 cd $(dirname $0)
 cd alpine
-
-PLATFORMS="linux/amd64 linux/arm64"
 
 for P in $PLATFORMS; do
     NAME=test-$(echo $P | tr / -)
